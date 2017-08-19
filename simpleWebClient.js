73fd01c6e00afe71191f11ca40067a5b454c1217
navono007 <navono007@gmail.com>
2017-08-19 21:21:00 +0800

    implement the web client and server

---
+++ simpleWebClient.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const queryString = require('querystring');
+
+const postData = queryString.stringify({
+  'msg': 'Hello World'
+});
+
+const options = {
+  hostname: 'localhost',
+  port: 8214,
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Length': postData.length
+  },
+  agent: false
+};
+
+const req = http.request(options, res => {
+  console.log(`STATUS: ${res.statusCode}`);
+  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+
+  // get data as chunks
+  res.on('data', chunk => {
+    console.log(`BODY: ${chunk}`);
+  });
+
+  // end response
+  res.on('end', _ => {
+    console.log(`No more data in response`);
+  });
+});
+
+req.on('error', e => {
+  console.log(`problem with request: ${e.message}`);
+});
+
+// write data to request body
+req.write(postData);
+req.end();
--- simpleWebServer.js
+++ simpleWebServer.js
@@ -1,15 +1,24 @@
 const http = require('http');
+const queryString = require('querystring');
+
 const server = http.createServer().listen(8214);
 
 server.on('request', (request, response) => {
-  // console.log(request.headers);
-  // console.log(request.rawHeaders);
+  if (request.method == 'POST') {
+    let body = '';
 
-  console.log(request.headers.host);
-  console.log(request.rawHeaders[0] + ' is ' + request.rawHeaders[1]);
-  
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
+    request.on('data', data => {
+      body += data;
+    });
+
+    request.on('end', _ => {
+      const post = queryString.parse(body);
+      console.log(post);
+
+      response.writeHead(200, {'Content-Type': 'text/plain'});
+      response.end('Hello World frome Server\n');
+    });
+  }
 });
 
 console.log('server listening on 8214');
